33676e0883f8a98e3b68975258c47c40063fe1f2
JoehIreri <mwendejoeh@gmail.com>
2023-06-18 12:13:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: 'get the last digit of a number'
+ * Return: always 0
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('\n');
+	}
+	putchar('\n');
+	return (0);
+}
